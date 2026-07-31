b9d115edcc789c40e0857e703a0e4fa2e7f09268
bryanster <45668775+bryanster@users.noreply.github.com>
2026-07-31 23:38:10 +0200

    rem: clean worktree for v2 build

--- internal/handler/fileutil.go
+++
@@ -1,9 +0,0 @@
-package handler
-
-import "os"
-
-// Standard file permission constants used throughout the handler package.
-const (
-	DirPerm  os.FileMode = 0o750
-	FilePerm os.FileMode = 0o600
-)
